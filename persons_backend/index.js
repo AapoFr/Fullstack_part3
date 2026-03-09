e4dfbfa9d28b338d8487d00f7d4342b45509a990
AapoFr <aapo.franzen@gmail.com>
2026-03-10 00:07:51 +0200

    move frontend

--- persons_backend/index.js
+++ persons_backend/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const app = express()
 const cors =require('cors')
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 morgan.token('body', function getBody(request,response){
@@ -88,7 +89,7 @@ app.post('/api/persons',(request,response) =>{
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
